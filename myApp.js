a95eb306145cf0674d4e2f9f3b51d3a04ac97f52
samuelbahiru <samuelbahiru93@gmail.com>
2023-12-27 10:34:44 +0300

    Part 1

--- myApp.js
+++ myApp.js
@@ -1,4 +1,12 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (req, res) => {
+    console.log("Db is connected");
+  }
+);
 
 let Person;
 
