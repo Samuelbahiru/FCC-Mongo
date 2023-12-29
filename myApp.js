9be94835ce2521c63c40a72a1a37cbf940354567
samuelbahiru <samuelbahiru93@gmail.com>
2023-12-29 11:06:40 +0300

    issue in database connection fix

--- myApp.js
+++ myApp.js
@@ -4,6 +4,8 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    ssl: true, // Enable TLS/SSL
+    sslValidate: true, // Validate the SSL certificate
   })
   .then((client) => {
     console.log("Db is connected");
